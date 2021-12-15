ceaf27a528cb1c07c158242d2e021f9467ce3369
Hank Shen <swh@admpub.com>
2021-12-15 13:52:40 +0800

    update

--- init.go
+++ init.go
@@ -1,8 +1,11 @@
 package sshmanager
 
 import (
+	"github.com/nging-plugins/sshmanager/pkg/handler"
 	_ "github.com/nging-plugins/sshmanager/pkg/library/setup"
 )
+
+var LeftNavigate = handler.LeftNavigate
 
 func init() {
 
