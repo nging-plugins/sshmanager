0f0ec0448a0ffe70c4de3b6fa894f1473208af5d
Hank Shen <swh@admpub.com>
2021-12-18 12:40:28 +0800

    update

---
+++ module.go
@@ -0,0 +1,20 @@
+package sshmanager
+
+import (
+	"github.com/admpub/nging/v4/application/library/module"
+
+	"github.com/nging-plugins/sshmanager/pkg/handler"
+	"github.com/nging-plugins/sshmanager/pkg/library/setup"
+)
+
+const ID = `term`
+
+var Module = module.Module{
+	TemplatePath: map[string]string{
+		ID: `servermanager/template/backend`,
+	},
+	AssetsPath:    []string{},
+	SQLCollection: setup.RegisterSQL,
+	Navigate:      RegisterNavigate,
+	Route:         handler.RegisterRoute,
+}
