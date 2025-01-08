134204982a51fb1aacf00347feb55bbf0d88550a
Hank Shen <swh@admpub.com>
2025-01-08 18:51:23 +0800

    update

--- application/handler/navigate.go
+++ application/handler/navigate.go
@@ -1,68 +1,71 @@
 package handler
 
-import "github.com/coscms/webcore/library/navigate"
+import (
+	"github.com/coscms/webcore/library/navigate"
+	"github.com/webx-top/echo"
+)
 
 var LeftNavigate = &navigate.Item{
 	Display: true,
-	Name:    `SSH管理`,
+	Name:    echo.T(`SSH管理`),
 	Action:  `term`,
 	Icon:    `terminal`,
 	Children: &navigate.List{
 		{
 			Display: true,
-			Name:    `账号管理`,
+			Name:    echo.T(`账号管理`),
 			Action:  `account`,
 		},
 		{
 			Display: true,
-			Name:    `添加账号`,
+			Name:    echo.T(`添加账号`),
 			Action:  `account_add`,
 			Icon:    `plus`,
 		},
 		{
 			Display: true,
-			Name:    `分组管理`,
+			Name:    echo.T(`分组管理`),
 			Action:  `group`,
 		},
 		{
 			Display: true,
-			Name:    `添加分组`,
+			Name:    echo.T(`添加分组`),
 			Action:  `group_add`,
 			Icon:    `plus`,
 		},
 		{
 			Display: false,
-			Name:    `修改账号`,
+			Name:    echo.T(`修改账号`),
 			Action:  `account_edit`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `删除账号`,
+			Name:    echo.T(`删除账号`),
 			Action:  `account_delete`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `修改分组`,
+			Name:    echo.T(`修改分组`),
 			Action:  `group_edit`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `删除分组`,
+			Name:    echo.T(`删除分组`),
 			Action:  `group_delete`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `SSH操作`,
+			Name:    echo.T(`SSH操作`),
 			Action:  `client`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `SFTP操作`,
+			Name:    echo.T(`SFTP操作`),
 			Action:  `sftp`,
 			Icon:    ``,
 		},
